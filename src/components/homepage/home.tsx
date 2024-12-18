fba6a9a80a5d290e5788c2f35b006c0d6bf15e45
Mohamad Mortada <mmortada685@gmail.com>
2024-12-17 20:07:28 -0800

    Update home.tsx

--- src/components/homepage/home.tsx
+++ src/components/homepage/home.tsx
@@ -248,7 +248,7 @@ export function HomeComponent() {
           <div id='contact' className="py-8 px-4 mx-auto w-max max-w-screen-xl lg:py-16 lg:px-6 bg-[#111]">
               <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
                   <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Team</h2>
-                  <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Explore the whole collection of open-source web components and elements built with the utility classes from Tailwind</p>
+                  <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Meet the team resposible for organizing these amazing hackathons!</p>
               </div> 
               <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
                       <div className="items-center rounded-lg shadow sm:flex  bg-[#222] dark:border-gray-700">
